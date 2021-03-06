10602d2155e5b58d8e2037a6624544835c37aa30
rahulsingh03 <47366017+rahulsingh03@users.noreply.github.com>
2021-03-06 11:58:19 +0530

    Update __init__.py
    remove unused modules

--- __init__.py
+++ __init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from decouple import config
 from flask_mail import Mail
 from flask_blog.config import Config
 
